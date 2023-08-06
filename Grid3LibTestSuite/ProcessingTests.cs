ee57d484f875857a1c731063f1848f9ccdca25cf
Al McNicoll <github@almcnicoll.co.uk>
2023-08-07 00:05:17 +0100

    Added mp3-refactoring code

--- Grid3LibTestSuite/ProcessingTests.cs
+++ Grid3LibTestSuite/ProcessingTests.cs
@@ -4,6 +4,9 @@ using System.IO.Compression;
 using System.IO;
 using System.Drawing.Imaging;
 using SixLabors.ImageSharp;
+using MediaToolkit;
+using MediaToolkit.Model;
+//using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Resources;
 
 namespace Grid3LibTestSuite
 {
@@ -15,8 +18,9 @@ namespace Grid3LibTestSuite
         {
             // Create test file by copying from source
             System.IO.File.Copy(@"G:\My Drive\Florence\Grid3\backup\grids\My Stories.gridset", @"G:\My Drive\Florence\Grid3\backup\grids\My Stories - Test.gridset.zip", true);
-            // Run process on test file
+            // Run JPEG process on test file
             GridSet.processFiles(@"G:\My Drive\Florence\Grid3\backup\grids\My Stories - Test.gridset.zip", wildcard: @"*.jp*", ProcessingTests.RecompressJPEG);
+            GridSet.processFiles(@"G:\My Drive\Florence\Grid3\backup\grids\My Stories - Test.gridset.zip", wildcard: @"*.mp*", ProcessingTests.RecompressMP3);
         }
 
         public static Stream RecompressJPEG(string filename, Stream input)
@@ -31,8 +35,7 @@ namespace Grid3LibTestSuite
                 // Scale down quality if it's higher than target quality
                 if (meta.Quality > quality)
                 {
-                    SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder jpegEncoder = new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder();
-                    jpegEncoder.Quality = quality;
+                    SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder jpegEncoder = new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder { Quality = quality };
                     originalImage.SaveAsJpeg(output, jpegEncoder);
                 }
                 else
@@ -44,20 +47,41 @@ namespace Grid3LibTestSuite
             }
         }
 
-        /*
+
         public static Stream RecompressMP3(string filename, Stream input)
         {
-            int quality = 60;
+            int quality = 96;
+            string tempSourceFile = Path.GetTempFileName();
+            string tempOutputFile = Path.GetTempFileName();
+
+            // FFmpeg.SetExecutablesPath(@"c:\program files\Shotcut\");
+
+            using (FileStream fileStream = new FileStream(tempSourceFile, FileMode.Create))
+            {
+                input.CopyTo(fileStream);
+            }
 
             Stream output = new MemoryStream();
-            using (Image originalImage = Image.Load(input))
+            var inputFile = new MediaFile { Filename = tempSourceFile };
+            using (Engine engine = new Engine())
             {
-                SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder jpegEncoder = new SixLabors.ImageSharp.Formats.Jpeg.JpegEncoder();
-                jpegEncoder.Quality = quality;
-                originalImage.SaveAsJpeg(output, jpegEncoder);
-                return output;
+                engine.GetMetadata(inputFile);
+
+                if (inputFile.Metadata.AudioData.BitRateKbs > quality)
+                {
+                    // If quality needs dropping, do so
+                    var outputFile = new MediaFile { Filename = tempOutputFile };
+                    engine.Convert(inputFile, outputFile);
+                    FileStream fsTemp = new FileStream(tempOutputFile, FileMode.Open, FileAccess.Read);
+                    fsTemp.CopyTo(output);
+                }
+                else
+                {
+                    // Just copy the stream
+                    input.CopyTo(output);
+                }
             }
+            return output;
         }
-        */
     }
 }
--- Grid3lib/CellImage.cs
+++ Grid3lib/CellImage.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Grid3lib.XmlNodeTag;
 using ImageSharp = SixLabors.ImageSharp;
 
